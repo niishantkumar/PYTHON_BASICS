0f99e6388b1b03afc1550df32aced0054a7f4cd2
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-08-09 21:33:23 +0530

    prac1.py modified

--- Conditionals/prac1.py
+++ Conditionals/prac1.py
@@ -1,1 +1,12 @@
 #wap to take input of marks and display grade
+
+marks = int(input("Enter marks : "))
+
+if(marks<=100 and marks>=90) :
+    print("Grade A")
+elif (marks<90 and marks>=80) :
+    print("Grade B")
+elif (marks<80 and marks>=70) :
+    print("Grade C")
+else :
+    print("Grade D")
